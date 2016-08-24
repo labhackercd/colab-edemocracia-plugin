a64537fb48ec5d2905155029c4bbf3496e8bcccd
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2016-08-24 17:43:19 -0300

    Fixed cache error
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_edemocracia/data_importer.py
+++ src/colab_edemocracia/data_importer.py
@@ -1,5 +1,3 @@
-from colab_wikilegis.models import WikilegisBill
-from colab_discourse.models import DiscourseTopic
 from colab.plugins.data import PluginDataImporter
 
 
@@ -8,9 +6,3 @@ class ColabEdemocraciaDataImporter(PluginDataImporter):
 
     def fetch_data(self):
         pass
-
-
-def get_home_data():
-    wikilegis_data = WikilegisBill.objects.all().order_by('-modified')
-    discourse_data = DiscourseTopic.objects.all().order_by('-last_posted_at')
-    return {'wikilegis_data': wikilegis_data, 'discourse_data': discourse_data}
--- src/colab_edemocracia/views.py
+++ src/colab_edemocracia/views.py
@@ -2,11 +2,71 @@ from django.views.generic import View
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.utils.http import is_safe_url
+from django.conf import settings
+from django.shortcuts import resolve_url
+from django.http import HttpResponseRedirect
+from django.contrib.auth import (REDIRECT_FIELD_NAME, login as auth_login)
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.response import TemplateResponse
+
 from .forms.accounts import SignUpForm
 from colab.accounts.models import EmailAddressValidation, EmailAddress
+from colab_wikilegis.models import WikilegisBill
+from colab_discourse.models import DiscourseTopic
 
 
 User = get_user_model()
+
+
+@sensitive_post_parameters()
+@csrf_protect
+@never_cache
+def login(request, template_name='registration/login.html',
+          redirect_field_name=REDIRECT_FIELD_NAME,
+          authentication_form=AuthenticationForm,
+          current_app=None, extra_context=None):
+    """
+    Displays the login form and handles the login action.
+    """
+    redirect_to = request.POST.get(redirect_field_name,
+                                   request.GET.get(redirect_field_name, ''))
+
+    if request.method == "POST":
+        form = authentication_form(request, data=request.POST)
+        if form.is_valid():
+
+            # Ensure the user-originating redirection url is safe.
+            if not is_safe_url(url=redirect_to, host=request.get_host()):
+                redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+
+            # Okay, security check complete. Log the user in.
+            auth_login(request, form.get_user())
+
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = authentication_form(request)
+
+    current_site = get_current_site(request)
+
+    wikilegis_data = WikilegisBill.objects.all().order_by('-modified')
+    discourse_data = DiscourseTopic.objects.all().order_by('-last_posted_at')
+
+    context = {
+        'form': form,
+        redirect_field_name: redirect_to,
+        'site': current_site,
+        'site_name': current_site.name,
+        'wikilegis_data': wikilegis_data,
+        'discourse_data': discourse_data,
+    }
+
+    return TemplateResponse(request, template_name, context,
+                            current_app=current_app)
 
 
 class SignUpView(View):
