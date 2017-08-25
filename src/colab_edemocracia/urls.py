7aa54251c41c15fa7fe78f47b87b3dbab58b42c2
erivanio <erivanio.vanin@gmail.com>
2017-08-25 15:44:02 -0300

    :sparkles: create form, view and url to sign on validation

--- src/colab_edemocracia/urls.py
+++ src/colab_edemocracia/urls.py
@@ -31,4 +31,6 @@ urlpatterns = patterns(
         TemplateView.as_view(template_name='widget/redirect.html'),
         name='redirect_widget'),
     url(r'^ajax/login/?$', views.ajax_login, name="ajax_login"),
+    url(r'^ajax/validation/?$', views.ajax_validation_signup,
+        name="ajax_validation"),
 )
