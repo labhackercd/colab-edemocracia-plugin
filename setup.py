964ee5b44f0072ec7fecfe27a62dc64c7b891755
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2017-11-08 14:48:23 -0200

    Bump version
    * New topbar
    
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_require = ['mock']
 
 setup(
     name="colab-edemocracia",
-    version='0.1.4',
+    version='0.2.0',
     author='labhackercd',
     author_email='labhackercd@gmail.com',
     url='https://github.com/labhackercd/colab-edemocracia-plugin',
